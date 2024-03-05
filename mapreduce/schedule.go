8f9bc8db6bf4563f14fe5ff62f4d0faf702e7233
adhammohamed1 <a.mohamed191100@gmail.com>
2024-03-05 15:54:23 +0200

    Implement schedule fn to support distributed map-reduce

--- mapreduce/schedule.go
+++ mapreduce/schedule.go
@@ -1,5 +1,9 @@
 package mapreduce
 
+import (
+	"sync"
+)
+
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
 	var numOtherPhase int
@@ -13,8 +17,40 @@ func (mr *Master) schedule(phase jobPhase) {
 	}
 	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, numOtherPhase)
 
-	// ToDo: Complete this function. See description in assignment.
+	// WaitGroup to wait for all tasks to complete
+	var wg sync.WaitGroup
 
+	for i := 0; i < ntasks; i++ {
+		// Increment the WaitGroup counter
+		wg.Add(1)
+
+		go func(taskNumber int) {
+			// Decrement the WaitGroup counter when the goroutine completes
+			defer wg.Done()
+
+			for {
+				// Get a worker from the registerChannel and assign the task to it
+				var workerAddress string = <-mr.registerChannel
+				taskArgs := &RunTaskArgs{
+					JobName:       mr.jobName,
+					File:          mr.files[taskNumber],
+					Phase:         phase,
+					TaskNumber:    taskNumber,
+					NumOtherPhase: numOtherPhase,
+				}
+
+				ok := call(workerAddress, "Worker.RunTask", taskArgs, new(struct{}))
+				if ok {
+					go func() {
+						mr.registerChannel <- workerAddress
+					}()
+					break
+				}
+			}
+		}(i) // Passing the task number to the goroutine
+	}
+
+	wg.Wait()
 
 	debug("Schedule: %v phase done\n", phase)
 }
